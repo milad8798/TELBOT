4a605269865f338553a2f94bdbe772ac0547a153
milad8798 <bankaidex0@gmail.com>
2025-07-30 17:58:00 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ TOKEN = "8384449381:AAGDJNJaLAZ0f983ZN5SiPx8v4LAk52kAjs"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [InlineKeyboardButton("دکمه ۱", callback_data="btn1")],
-        [InlineKeyboardButton("دکمه ۲", callback_data="btn2")]
+        [InlineKeyboardButton("مولتی 1", callback_data="btn1")],
+        [InlineKeyboardButton("بتل 2", callback_data="btn2")],
+        
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("سلام! یکی از دکمه‌ها رو انتخاب کن:", reply_markup=reply_markup)
+    await update.message.reply_text("مود دلخواه رو انتخاب کن:", reply_markup=reply_markup)
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
