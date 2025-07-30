3234c83c9511cdd88d94c0172b668f12924421ab
milad8798 <bankaidex0@gmail.com>
2025-07-30 10:00:49 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import telebot
+
 TOKEN = "8252909254:AAF6Guzpm18fb3K8puNq4zp-eDBeYqOXCE4"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "ربات روشن شد ✅")
+
+print("Bot is running...")
+bot.polling(none_stop=True)
